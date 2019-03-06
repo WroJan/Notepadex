2a4bef6e4eb9d2e54015b46541b091d02209c5f9
janek <janek93.wroblewski@gmail.com>
2019-03-06 16:21:47 +0000

    First Commit, no changes yet, just empty upload

---
+++ app/src/androidTest/java/com/janWroblewski/notepadex/ExampleInstrumentedTest.kt
@@ -0,0 +1,24 @@
+package com.janWroblewski.notepadex
+
+import android.support.test.InstrumentationRegistry
+import android.support.test.runner.AndroidJUnit4
+
+import org.junit.Test
+import org.junit.runner.RunWith
+
+import org.junit.Assert.*
+
+/**
+ * Instrumented test, which will execute on an Android device.
+ *
+ * See [testing documentation](http://d.android.com/tools/testing).
+ */
+@RunWith(AndroidJUnit4::class)
+class ExampleInstrumentedTest {
+    @Test
+    fun useAppContext() {
+        // Context of the app under test.
+        val appContext = InstrumentationRegistry.getTargetContext()
+        assertEquals("com.janWroblewski.notepadex", appContext.packageName)
+    }
+}
