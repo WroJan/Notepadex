b41b9b564649cb3ddba28e7093101549305ed16d
janek <janek93.wroblewski@gmail.com>
2019-03-06 16:49:30 +0000

    added adapter view class with no content yet.

--- app/src/main/java/com/janWroblewski/notepadex/MainActivity.kt
+++ app/src/main/java/com/janWroblewski/notepadex/MainActivity.kt
@@ -2,12 +2,17 @@ package com.janWroblewski.notepadex
 
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.support.v7.widget.LinearLayoutManager
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        recycler_view_notes.layoutManager = LinearLayoutManager(this)
+        recycler_view_notes.adapter
 
 
 
