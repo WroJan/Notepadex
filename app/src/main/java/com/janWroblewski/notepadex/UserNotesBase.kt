962f18a0635f258f0783e6eb7773ca1c08e69e2a
janek <janek93.wroblewski@gmail.com>
2019-03-06 16:36:25 +0000

    noterow wos deleted, not working correctly. Replaced with note_row

---
+++ app/src/main/java/com/janWroblewski/notepadex/UserNotesBase.kt
@@ -0,0 +1,8 @@
+package com.janWroblewski.notepadex
+
+object UserNotesBase {
+
+    var noteTitle :ArrayList<String>  = arrayListOf<String>()
+
+    var noteBody : ArrayList<String> = arrayListOf<String>()
+}
